94a9b093ffa6d2c7e37aa4b38358e0aba171eb79
Noneya Bisnus <cmkroop@gmail.com>
2024-04-06 00:19:25 -0400

    UI, Sounds, & Music

--- my_project/code/HealthTrigger.cs
+++ my_project/code/HealthTrigger.cs
@@ -12,6 +12,7 @@ public sealed class HealthTrigger : Component, Component.ITriggerListener
 		{
 			player.Health += Amount;
 			player.Health = Math.Clamp( player.Health, 0, player.MaxHealth );
+			Sound.Play("sounds/hurt.sound");
 		}
 	}
 
